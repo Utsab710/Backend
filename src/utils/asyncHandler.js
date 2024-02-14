515122aee224e3bf7947167d4f770beef5343905
Utsab <utsabsshrestha710@gmail.com>
2024-02-14 18:17:06 +0545

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+//This is promise method
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// yo try, catch wala method part
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(res, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
